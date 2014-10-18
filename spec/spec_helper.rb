f7e5fa0b007e88400719814f59f40c4d8192fa4e
rayc5 <rayc5c5@gmail.com>
2014-10-18 19:23:07 +0900

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -39,4 +40,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #Capybaraのvisitがundefinedと言われたとき、Capybara::DSLを含めるようにしたらうまくいった
+  config.include Capybara::DSL
+
 end
